6d5469f8420d3ceb010026f31ab1ca1b20c5f3c6
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-13 15:00:46 -0800

    build App compnent by passing in props to QuestionCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true)
+
   const startTrivia = async () => {
 
   }
@@ -24,7 +34,11 @@ const App = () => {
         Score: 
       </p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard 
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
-const QuestionCard = () => {
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer,questionNum, totalQuestions}) => {
   return (
-    <div>Question Card</div>
+    <div>
+      <p className="number">
+        Question: {questionNum} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.maps(answer => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={ {__html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
